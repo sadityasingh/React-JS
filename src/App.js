b1eb26fc2fc28d14df3e9f42b827cbbc54ea363d
sadityasingh <info.adityasinghparihar@gmail.com>
2020-09-20 21:18:49 +0530

    first Commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0); //Set is respomnsible for updateing the variable
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Counter App using State/Hooks</h1>
+      </header>
+      <h2>Current value of count is {count}</h2>
+      <button
+        onClick={() => {
+          setCount(0);
+        }}
+      >
+        Reset Counter
+      </button>
+      <button onClick={() => (count > 9 ? "" : setCount(count + 1))}>
+        Increase Counter
+      </button>
+      <button onClick={() => (count < 1 ? "" : setCount(count - 1))}>
+        Decrease Counter
+      </button>
+    </div>
+  );
+}
+
+export default App;
